07add0322c91d5d24639e64e5d3f54d9ba4c3aef
juliusbay <juax0001@stud.kea.dk>
2025-11-24 16:45:16 +0100

    small change to fix userRepository

--- src/main/java/dk/ek/bcrafteksamensprojekt/repository/UserRepository.java
+++ src/main/java/dk/ek/bcrafteksamensprojekt/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package dk.ek.bcrafteksamensprojekt.repository;
 
-import org.apache.catalina.User;
+import dk.ek.bcrafteksamensprojekt.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
