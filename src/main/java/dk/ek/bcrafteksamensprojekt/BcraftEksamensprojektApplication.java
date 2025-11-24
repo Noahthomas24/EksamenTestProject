61f899ed92dfea95431873344982f38d9c9650c9
juliusbay <juax0001@stud.kea.dk>
2025-11-24 10:47:11 +0100

    initial commit

---
+++ src/main/java/dk/ek/bcrafteksamensprojekt/BcraftEksamensprojektApplication.java
@@ -0,0 +1,13 @@
+package dk.ek.bcrafteksamensprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcraftEksamensprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcraftEksamensprojektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/ek/bcrafteksamensprojekt/BcraftEksamensprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ek.bcrafteksamensprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcraftEksamensprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
